ef780ab42b246217d56f0c8b6a5807b919c1f2a2
Anny Confessor <anny.confessor@gmail.com>
2021-07-20 14:53:30 -0300

    feat: add routes and My404Page to route not found

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -6,11 +6,13 @@ import {
   } from "react-router-dom";
 
 import Home from './screens/Home'
+import My404Page from "./screens/My404Page";
 
 const Routes = (): React.ReactElement => (
     <Router>
         <Switch>
-            <Route path="/" component={Home}/>
+            <Route exact path="/" component={Home}/>
+            <Route path="*" exact={true} component={My404Page}/>
         </Switch>
     </Router>
 )
---
+++ src/screens/My404Page.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const My404Page: React.FC = () => <div>Eu sou a My404Page</div>
+
+export default My404Page;
