1a988d37cceb0fd45368de642a4b28be5f3f58be
Anny Confessor <anny.confessor@gmail.com>
2021-07-24 11:05:16 -0300

    feat: theme and reset css

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ThemeProvider } from 'styled-components';
 
 import reportWebVitals from './reportWebVitals';
 import Routes from './Routes';
+import GlobalStyle from './globalStyle'
+import theme from './theme'
 
 ReactDOM.render(
   <React.StrictMode>
-    <Routes />
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Routes />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
---
+++ src/theme.tsx
@@ -0,0 +1,12 @@
+const theme = {
+    colors: {
+        lightRed: "#D30026",
+        darkRed: "#9D152C",
+        lightGray: "#C5CFD1",
+        gray: "#868A93",
+        darkGray: "#5E6674",
+        white: "#FFFFFF"
+    }
+}
+
+export default theme
